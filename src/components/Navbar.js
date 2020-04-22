01f7bc7283c691f93f3f9d83396f925149455508
AndrewMatheny <androidmatheny@gmail.com>
2020-04-21 17:29:47 -0700

    scrapped redirects, trying Navlinks - Home stays highlighted - need to fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,33 +11,40 @@ export default class Navbar extends Component {
 
   handleItemClick = (e, { name }) => {
     
-    this.setState({ activeItem: name, redirect: name })
+    this.setState({
+       activeItem: name,
+      //  redirect: name 
+    })
 
 
   }
 
   render() {
     const { activeItem } = this.state
-    if (this.state.redirect) {
-      return <Redirect to={this.state.redirect} />
-    }
+    // if (this.state.redirect) {
+    //   return <Redirect to={this.state.redirect} />
+    // }
 
     return (
       <div>
         <Menu pointing secondary>
           <Menu.Item
-            // as={NavLink}
-            // to='/'
+            as={NavLink}
+            to='/'
             name='home'
             active={activeItem === 'home'}
             onClick={this.handleItemClick}
           />
           <Menu.Item
+            as={NavLink}
+            to='/about'
             name='about'
             active={activeItem === 'about'}
             onClick={this.handleItemClick}
           />
           <Menu.Item
+            as={NavLink}
+            to='/dailydollar'
             name='daily dollar'
             active={activeItem === 'dailydollar'}
             onClick={this.handleItemClick}
