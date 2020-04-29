a9013bf335e85ea5e529d4530336d573a5d4f210
AndrewMatheny <androidmatheny@gmail.com>
2020-04-28 21:05:50 -0700

    cleanup

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,9 @@
 import React from 'react'
-// import NavBar from '../components/Navbar'
 
 export default class Home extends React.Component {
     render() {
         return (
             <div>
-            {/* < NavBar /> */}
             <div>Home Page</div>
             </div>
         )
