29f74007b5b7e65f2671c1e541bd597a6786479a
AndrewMatheny <androidmatheny@gmail.com>
2020-05-02 14:17:56 -0700

    home container

---
+++ src/containers/HomeContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class HomeContainer extends React.Component {
+    render() {
+        return (
+            <div>
+            <p>This is the home page Container</p>
+            </div>
+        )
+    }
+}
