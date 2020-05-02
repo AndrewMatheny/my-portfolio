bbf2bcb04a30ba55c1aec59b528e2ba0c848f8fe
AndrewMatheny <androidmatheny@gmail.com>
2020-05-02 14:19:26 -0700

    imported home container

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import HomeContainer from '../containers/HomeContainer'
 
 export default class Home extends React.Component {
     render() {
         return (
             <div>
             <div>Home Page</div>
-            <p>This is the home page Component holder</p>
+            <HomeContainer />
             </div>
         )
     }
