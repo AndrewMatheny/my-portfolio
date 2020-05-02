c65a018744c9076c00e27a68133ee4a16893c72f
AndrewMatheny <androidmatheny@gmail.com>
2020-05-01 23:03:52 -0700

    placeholder for home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,7 @@ export default class Home extends React.Component {
         return (
             <div>
             <div>Home Page</div>
+            <p>This is the home page Component holder</p>
             </div>
         )
     }
