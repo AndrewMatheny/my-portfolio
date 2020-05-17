e5bc2eb8cbb33914d7342cf55ebb53ebb383cfce
AndrewMatheny <androidmatheny@gmail.com>
2020-05-16 20:18:17 -0700

    basic layout for information card

---
+++ src/containers/InformationCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class InformationCard extends React.Component {
+    render() {
+        return (
+            <div>
+            <p>This is the information card container</p>
+            </div>
+        )
+    }
+}
