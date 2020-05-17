d9bee9cab9182cf32beb6883f32360349d29d329
AndrewMatheny <androidmatheny@gmail.com>
2020-05-17 08:28:14 -0700

    imported information car into Trip Planner page

--- src/components/TripPlanner.js
+++ src/components/TripPlanner.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import InformationCard from '../containers/InformationCard'
 
 export default class TripPlanner extends React.Component {
     render() {
         return (
-            <div>Trip Planner Page</div>
+            <div>
+                Trip Planner Page
+                <InformationCard />
+
+            </div>
         )
     }
 }
